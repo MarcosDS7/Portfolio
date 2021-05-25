5db1b6bfbb18e8379055506e32a5a22c117dc82b
Marcos Vinícius Rodrigues de Souza <marcossouza.0ds@gmail.com>
2021-05-24 22:20:45 -0300

    Desenvolvimento do responsivo das seções

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Eae gatão')
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+	if(event.type === 'touchstart') event.preventDefault()
+	const nav = document.getElementById('nav');
+	
+	nav.classList.toggle('active');
+	const active = nav.classList.contains('active')
+	event.currentTarget.setAttribute('aria-expanded', active);
+
+	if (active) {
+		event.currentTarget.setAttribute('aria-label', 'Fechar menu')
+	} else {
+		event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+	} 
+}
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
